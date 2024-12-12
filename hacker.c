2e1814452e5dc456218eb2d9359fb99b0858a483
Osvaldo Hernandez <osvaldo23hs@gmail.com>
2024-12-11 20:38:21 -0500

    Implemented fully functional hacker node in hacker.c.

--- hacker.c
+++ hacker.c
@@ -1,43 +1,49 @@
 /*
-Description: 
+Description: Hacker node which sends a message to server when summoned and is not part of the Ricart-Agrawala algorithm.
 Author: Osvaldo Hernandez-Segura
-Depedencies: 
-*/
-
-/* NOTES
-1. The nodes will print to a shared memory, which, for the chosen node, will then be sent to the server to print to the terminal.
-2. The hacker will not communicate with the other nodes; it will act on its own and send a print whenever it is summoned.
 */
 
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <string.h>
-# include <semaphore.h>
+# include <sys/ipc.h> // ipc key generation
+# include <sys/msg.h> // message queue functions
+# include <sys/types.h> 
 
-typedef struct hacker {
-    int N = 0; // number of nodes
-    int request_number; // nodes sequence number
-    int highest_request_number; // highest request number seen
-    int outstanding_reply; // # of outstanding replies
+# define BUFFER_SIZE 50
+
+/*
+Message queue buffer.
+ */
+struct msgbuf {
+    long msg_type; // type of message
+    char msg_text[BUFFER_SIZE]; // message buffer for text of message
 };
 
-
-
-int main(int argc, char *argv[]) {
-
-    int n = runNetwork();
-
-    exit(n);
+/*
+Gets an existing message queue from the kernel.
+*/
+int get_message_queue() {
+    // init message parameters
+    int msgq_id; // message queue id var
+    int msgflg = IPC_CREAT | 0666; // message flag
+    int i = 'z'; 
+    key_t key = ftok(".", i); // key for message queue
+    int msg_id;
+    if ((msg_id = msgget(key, msgflg)) < 0) { // error for mssgget
+        perror("Message queue error:");
+        return 1;
+    }
+    printf("Message queue %d attained successfully!\n", msg_id);
+    return msg_id;
 }
 
-// example output: ### START OUTPUT FOR NODE 4 ###
-//4 this is line 1
-//4this is line 2
-//-- END OUTPUT FOR NODE 4 --
-//### START OUTPUT FOR NODE 3 ###
-//3 first lie of output
-//HACKER Ja ja ja...
-//3 hello world
-//-- END OUTPUT FOR NODE 3 --
-//......
+int main(int argc, char *argv[]) {
+    int msg_id = get_message_queue();
+    struct msgbuf msg_buf;
+    msg_buf.msg_type = 3; // type 3 is for node to server message
+    sprintf(msg_buf.msg_text, "HACKER Ja ja ja...\n");
+    if (msgsnd(msg_id, &msg_buf, BUFFER_SIZE, IPC_NOWAIT) < 0) perror("Message could not be sent to server.\n");
+    printf("Message %s sent to server!\n", msg_buf.msg_text);
+    exit(0);
+}
